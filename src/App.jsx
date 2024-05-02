1e0adc074d0a511759d3d257243d0b4f0417ebdd
Yevhenii Sarancha <yevhenii.sarancha@outlook.com>
2024-05-02 21:32:32 +0300

    preset components

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,17 @@
 import "./App.css";
 
+import Description from "./components/Description/Description";
+import Options from "./components/Options/Options";
+import Feedback from "./components/Feedback/Feedback";
+
 const App = () => {
-  return <div className="container"></div>;
+  return (
+    <div className="container">
+      <Description></Description>
+      <Options></Options>
+      <Feedback></Feedback>
+    </div>
+  );
 };
 
 export default App;
