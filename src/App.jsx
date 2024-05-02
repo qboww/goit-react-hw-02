ef36462a8ea9a75c82ab13ab67470f72edfe4b27
Yevhenii Sarancha <yevhenii.sarancha@outlook.com>
2024-05-02 21:20:11 +0300

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,7 @@
 import "./App.css";
 
-import Profile from "./components/Profile/Profile";
-import FriendList from "./components/FriendList/FriendList";
-import TransactionHistory from "./components/TransactionHistory/TransactionHistory";
-
-import userData from "./data/userData.json";
-import friends from "./data/friends.json";
-import transactions from "./data/transactions.json";
-
 const App = () => {
-  return (
-    <div className="container">
-      <Profile {...userData} />
-      <FriendList friends={friends} />
-      <TransactionHistory items={transactions} />
-    </div>
-  );
+  return <div className="container"></div>;
 };
 
 export default App;
