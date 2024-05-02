4df2aa7ed84ea246bdb68bca73dc8530b730ca38
Yevhenii Sarancha <yevhenii.sarancha@outlook.com>
2024-05-02 22:23:10 +0300

    minor visual fixes

--- src/App.jsx
+++ src/App.jsx
@@ -31,7 +31,7 @@ const App = () => {
   };
 
   return (
-    <div>
+    <div className="container">
       <Description />
       <Options updateFeedback={updateFeedback} />
       {totalFeedback > 0 && (
