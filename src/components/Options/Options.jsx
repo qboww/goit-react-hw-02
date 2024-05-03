cce25010c55021cce78776bc3588ab275ed3519b
Yevhenii Sarancha <yevhenii.sarancha@outlook.com>
2024-05-03 09:36:23 +0300

    minor fix

--- src/components/Options/Options.jsx
+++ src/components/Options/Options.jsx
@@ -6,7 +6,11 @@ const Options = ({ totalFeedback, updateFeedback, handleReset }) => {
       <button onClick={() => updateFeedback("good")}>Good</button>
       <button onClick={() => updateFeedback("neutral")}>Neutral</button>
       <button onClick={() => updateFeedback("bad")}>Bad</button>
-      {totalFeedback > 0 && <button onClick={handleReset}>Reset</button>}
+      {totalFeedback > 0 && (
+        <button onClick={handleReset} className="button-reset">
+          Reset
+        </button>
+      )}
     </div>
   );
 };
